f54c0acc05bc4d0ba09ca4bb708000e16c2d3de2
Havard52 <103666284+Havard52@users.noreply.github.com>
2023-12-16 15:40:45 +0100

    img 1

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,43 @@
-const projectScreenshot = document.querySelector(".screenshot");
-const popUpScreenshot = document.querySelector(".popupScreenshot");
+const rainydaysScreen = document.getElementById ("screenshotRainydays");
+const museumScreen = document.getElementById ("screenshotMuseum");
+const MealScreen = document.getElementById ("screenshotMealPreppers");
+const popUpRainy = document.getElementById("RainyDaysScreen");
+const popUpMuseum = document.getElementById("museumScreen");
+const popUpMeal = document.getElementById("mealScreen");
 
-function screenOnclick 
+
+
+rainydaysScreen.onclick = function() {
+    popUpRainy.style.display = "block";
+
+    const closeImageModule = document.querySelector ("#closeImageModuleRainy");
+
+    closeImageModule.onclick = function () {
+        popUpRainy.style.display = "none";
+    };
+};
+
+museumScreen.onclick = function() {
+    popUpMuseum.style.display = "block";
+
+    const closeImageModule = document.querySelector ("#closeImageModuleMuseum");
+
+    closeImageModule.onclick = function () {
+        popUpMuseum.style.display = "none";
+    };
+};
+
+MealScreen.onclick = function() {
+    popUpMeal.style.display = "block";
+
+    const closeImageModule = document.querySelector ("#closeImageModuleMeal");
+
+    closeImageModule.onclick = function () {
+        popUpMeal.style.display = "none";
+    };
+};
+
+
+
+
+
